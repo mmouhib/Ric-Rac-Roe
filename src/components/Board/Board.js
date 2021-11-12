36c49a5c628c6f84b95055db5c1dc984f6bbbab9
mmouhib <mouhibouni321@gmail.com>
2021-11-12 23:49:06 +0100

    enhanced UI

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,22 +1,20 @@
 import './Board.css';
 
-function handleHover() {
-	if (!(this.innerHTML === '')) {
-		this.classList.add('');
-	}
-}
-
 function Board() {
 	return (
 		<div className="board">
+			<div className="title">
+				<h1>Tic Tac Toe</h1>
+			</div>
+
 			<table>
 				<tr>
 					<td></td>
-					<td class="vert">x</td>
+					<td class="vert"></td>
 					<td></td>
 				</tr>
 				<tr>
-					<td class="hori">x</td>
+					<td class="hori"></td>
 					<td class="vert hori"></td>
 					<td class="hori"></td>
 				</tr>
@@ -26,6 +24,15 @@ function Board() {
 					<td></td>
 				</tr>
 			</table>
+
+			<div className="score">
+				<p id="p1">
+					Player 1: <span>6</span>
+				</p>
+				<p>
+					Player 2: <span>1</span>
+				</p>
+			</div>
 		</div>
 	);
 }
