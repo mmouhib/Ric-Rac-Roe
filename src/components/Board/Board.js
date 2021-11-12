d177bf24dd3ba8896e144bd9cd24a7446bd3f8e1
mmouhib <mouhibouni321@gmail.com>
2021-11-12 22:16:52 +0100

    generated board style

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,23 +1,29 @@
 import './Board.css';
 
+function handleHover() {
+	if (!(this.innerHTML === '')) {
+		this.classList.add('');
+	}
+}
+
 function Board() {
 	return (
 		<div className="board">
 			<table>
 				<tr>
-					<td>x</td>
+					<td></td>
 					<td class="vert">x</td>
-					<td>x</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td class="hori">x</td>
-					<td class="vert hori">x</td>
-					<td class="hori">x</td>
+					<td class="vert hori"></td>
+					<td class="hori"></td>
 				</tr>
 				<tr>
-					<td>x</td>
-					<td class="vert">x</td>
-					<td>x</td>
+					<td></td>
+					<td class="vert"></td>
+					<td></td>
 				</tr>
 			</table>
 		</div>
